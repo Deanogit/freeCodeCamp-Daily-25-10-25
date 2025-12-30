ce591531515a9f539ff92ae5f986a46b68b241e8
Dean Howe <hideanhowe@gmail.com>
2025-12-30 21:40:57 +0000

    testing how to get the value from the key

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
 // The letters "A" and "T" complement each other.
 // The letters "C" and "G" complement each other.
 // For example, given "ACGT", return "TGCA".
+
+function complementaryDNA(strand) {
+  console.log(strand);
+
+  // set up an object;
+  const ref = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C',
+  };
+
+  const arr = strand.split('');
+  console.log(arr);
+  arr.map((x) => {
+    // if ref.includes(x) {
+    // swap the key for value
+    //}
+  });
+  return strand;
+}
